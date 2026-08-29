cf1e273310286d886745f16fc76b55bd97e87148
saeedkamyab <saeed.kamyab@yahoo.com>
2026-08-29 11:50:07 +0330

    feat(Payment): Domain payment Agg added

---
+++ services/Payment/Payment.Domain/Enums/PaymentStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Payment.Domain.Enums;
+
+
+public enum PaymentStatus
+{
+    Pending = 1,
+    Succeeded = 2,
+    Failed = 3
+}
---
+++ services/Payment/Payment.Domain/Payments/Payment.cs
@@ -0,0 +1,73 @@
+﻿using Payment.Domain.Enums;
+
+namespace Payment.Domain.Payments;
+
+public sealed class Payment
+{
+    public Guid Id { get; private set; }
+
+    public Guid OrderId { get; private set; }
+
+    public decimal Amount { get; private set; }
+
+    public PaymentStatus Status { get; private set; }
+
+    public DateTime CreatedOnUtc { get; private set; }
+
+    public DateTime? CompletedOnUtc { get; private set; }
+
+    private Payment()
+    {
+    }
+
+    private Payment(
+        Guid id,
+        Guid orderId,
+        decimal amount)
+    {
+        if (orderId == Guid.Empty)
+            throw new ArgumentException(
+                "Order id cannot be empty.",
+                nameof(orderId));
+
+        if (amount <= 0)
+            throw new ArgumentOutOfRangeException(
+                nameof(amount),
+                "Payment amount must be greater than zero.");
+
+        Id = id;
+        OrderId = orderId;
+        Amount = amount;
+        Status = PaymentStatus.Pending;
+        CreatedOnUtc = DateTime.UtcNow;
+    }
+    public static Payment Create(
+       Guid orderId,
+       decimal amount)
+    {
+        return new Payment(
+            Guid.NewGuid(),
+            orderId,
+            amount);
+    }
+
+    public void MarkSucceeded()
+    {
+        if (Status != PaymentStatus.Pending)
+            throw new InvalidOperationException(
+                $"Cannot succeed payment in status {Status}.");
+
+        Status = PaymentStatus.Succeeded;
+        CompletedOnUtc = DateTime.UtcNow;
+    }
+
+    public void MarkFailed()
+    {
+        if (Status != PaymentStatus.Pending)
+            throw new InvalidOperationException(
+                $"Cannot fail payment in status {Status}.");
+
+        Status = PaymentStatus.Failed;
+        CompletedOnUtc = DateTime.UtcNow;
+    }
+}
