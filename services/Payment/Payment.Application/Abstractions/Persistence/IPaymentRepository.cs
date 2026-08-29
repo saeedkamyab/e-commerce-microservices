d8ee7498acde17796a2e7f171e7710763f20f7cc
saeedkamyab <saeed.kamyab@yahoo.com>
2026-08-29 12:35:39 +0330

    feat(Payment): Persistence Abstaction added

---
+++ services/Payment/Payment.Application/Abstractions/Persistence/IPaymentRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Payment.Application.Abstractions.Persistence;
+
+public interface IPaymentRepository
+{
+    Task<Domain.Payments.Payment?> GetByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+
+    Task AddAsync(
+        Domain.Payments.Payment payment,
+        CancellationToken cancellationToken = default);
+}
---
+++ services/Payment/Payment.Application/Abstractions/Persistence/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace Payment.Application.Abstractions.Persistence;
+
+public interface IUnitOfWork
+{
+    Task<int> SaveChangesAsync(
+        CancellationToken cancellationToken = default);
+}
